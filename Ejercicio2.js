7b68299dcb7319be68729394a158d22d0a138302
dyvaDColes <107653198+dyvaDColes@users.noreply.github.com>
2022-10-05 18:24:21 +0200

    Add files via upload

---
+++ Ejercicio2.js
@@ -0,0 +1,17 @@
+const nacimiento = new Date("6 July 1992");
+const libro = {
+    titulo: "La Perla",
+    Autor: "John Steinbeck",
+    Fecha: 1947,
+    URL: "https://es.wikipedia.org/wiki/La_perla_(novela)"
+};
+
+const myInfo = [
+    "David",
+    30,
+    false,
+    nacimiento,
+    libro    
+]
+
+console.log(myInfo)
